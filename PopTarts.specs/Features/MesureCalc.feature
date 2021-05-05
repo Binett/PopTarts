0667b0d5268df2b103726d9a90fb53ae21d37e72
Tobias Binett <tobias_binett@hotmail.se>
2021-05-05 13:34:54 +0200

    Added MesurmentCalc And StepDef

---
+++ PopTarts.specs/Features/MesureCalc.feature
@@ -0,0 +1,10 @@
+﻿Feature: MesureCalc
+	As an amateur baker 
+	I want to be able to convert from American measurements to Swedish
+	so that I can know what I can use
+
+Scenario: Calculate Mesurments
+	Given that I need an American cup
+	And the decimals are rounded to 1
+	When the amount of cups is 1
+	Then the Swedish amount is 2.4 dl
---
+++ PopTarts.specs/Steps/MesureCalcSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PopTarts.specs.Steps
+{
+    [Binding]
+    public class MesureCalcSteps
+    {
+        [Given(@"that I need an American cup")]
+        public void GivenThatINeedAnAmericanCup()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the decimals are rounded to (.*)")]
+        public void GivenTheDecimalsAreRoundedTo(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the amount of cups is (.*)")]
+        public void WhenTheAmountOfCupsIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Swedish amount is (.*) dl")]
+        public void ThenTheSwedishAmountIsDl(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
