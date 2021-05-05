84b008de2ca127b8cca3a9eefbc28aa055a78d1f
Nicklas Eriksson <salk1n@hotmail.com>
2021-05-05 14:09:59 +0200

    ConvertToDL done

--- PopTarts/Recepies/Convertion.cs
+++ PopTarts/Recepies/Convertion.cs
@@ -6,9 +6,9 @@ namespace PopTarts.Recepies
 {
     public static class Convertion
     {
-        public static void ConvertToDL(Decimal cups) //Nicklas + Tobias
+        public static float ConvertToDL(float cups) //Nicklas + Tobias
         {
-            
+           return MathF.Round(cups * 2.36588237F, 1);
         } 
 
         public static void RoundToOneDecimal()//
