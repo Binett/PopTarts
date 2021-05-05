83ef44d35f6a2641ee701874a9aed3f0114191c3
Tobias Binett <tobias_binett@hotmail.se>
2021-05-05 12:08:05 +0200

    Add project files.

---
+++ PopTarts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PopTarts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
